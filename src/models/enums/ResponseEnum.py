3c8d7e1b5c68cc87f0839f92e9f16cab7cedc01c
Saied <saied1412002@gmail.com>
2025-01-24 07:44:06 +0200

    add to response enums

--- src/models/enums/ResponseEnum.py
+++ src/models/enums/ResponseEnum.py
@@ -6,3 +6,5 @@ class ResponseSignals(Enum):
     FILE_UPLOADED_FAILED="file_uploaded_failed"
     File_validate_success="file_validate_success"
     File_validate_failed="file_validate_failed"
+    PROCESSING_SUCCESS="processing_success"
+    PROCESSING_FAILED="processing_failed"
