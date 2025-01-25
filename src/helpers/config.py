043f457ee113028573e26c0482a6a45aeb7ebe9e
Saied <saied1412002@gmail.com>
2025-01-25 08:00:09 +0200

    add new app settings

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -3,9 +3,15 @@ class Settings(BaseSettings):
     APP_NAME:str
     APP_VERSION:str
     OPENAI_KEY:str
+
+    #!File uploading settings
     FILE_ALLOWED_TYPES:list
     FILE_MAX_SIZE:int
     FILE_CHUNK_SIZE:int
+
+    #^database settings
+    MONGODB_URL:str
+    MONGODB_DATABASE:str
 
     class config :
         env_file=".env"
