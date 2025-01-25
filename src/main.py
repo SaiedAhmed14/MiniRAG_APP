8335f4e7c992691b8231cfcd10e1cfe0149522ce
Saied <saied1412002@gmail.com>
2025-01-25 07:59:36 +0200

    add start and shutdown db connection events

--- src/main.py
+++ src/main.py
@@ -2,6 +2,26 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 load_dotenv(".env")
 from routes import base,data
+from motor.motor_asyncio import AsyncIOMotorClient
+from helpers.config import get_settings
+
+#* FastAPI app initialization
 app=FastAPI()
+
+
+#& setting up the database client
+@app.on_event("startup")
+async def startup_db_client():
+    settings=get_settings()
+    app.mongo_conn=AsyncIOMotorClient(settings.MONGODB_URL)
+    app.db_client=app.mongo_conn[settings.MONGODB_DATABASE]
+
+#& shutting down the database client
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongo_conn.close()
+
+
+
 app.include_router(base.base_router)
 app.include_router(data.data_router)
